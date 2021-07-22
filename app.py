21ccfaa945e2dea27962ef43d53a488d85aa53f0
pappulasathvik <83030990+pappulasathvik@users.noreply.github.com>
2021-07-22 19:06:32 +0530

    Update app.py
    Hi

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello,Lakshmikanth, What are you doing and learn datascience"
+    return "Hello,Lakshmikanth, What are you doing. don't waste the time learn datascience"
